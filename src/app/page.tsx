0cef622f4d16c538830edaa139139ea1ee5269bc
PedroHQC <pedrohenrique2.0583@gmail.com>
2025-05-02 00:14:41 -0300

    fix: responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,34 +35,36 @@ export default function Home() {
         }}
       >
         <div className="flex flex-col h-full w-full">
-          <div className="flex justify-between mt-0 mb-0 !h-[7.5%] w-full bg-gradient-to-b from-[#D2D3D7] to-[#CCCCCC]">
+          <div className="flex justify-between mt-0 mb-0 xl:h-22 h-[250px] w-full bg-gradient-to-b from-[#D2D3D7] to-[#CCCCCC]">
             <span className="ml-5 h-auto text-3xl text-[#396291] flex items-center gap-4 w-full">
-              <div className="h-[89.54%] w-[3.36%] border-white border-2 rounded-[50%] overflow-hidden">
+              <div className="xl:h-15 xl:w-15 h-[180px] w-[180px] border-white border-8 xl:border-2 rounded-[50%] overflow-hidden">
                 <Image alt="" src={ProfileImage} width={0} height={0} className="relative bottom-2.5 !w-full" />
               </div>
-              <span className="w-[calc(100%-64px)] border-b-2 text-[clamp(22px,2vw,2rem)] font-[family-name:var(--font-geist-mono)]">
+              <span className="w-[calc(100%-64px)] border-b-8 text-7xl xl:text-3xl xl:border-b-2 font-[family-name:var(--font-geist-mono)]">
                 PEDRO HQC_
               </span>
             </span>
           </div>
-          <div className="flex flex-row gap-[250px] overflow-hidden h-full items-center">
-            <Image alt="" src={ProImage} width={0} height={0} className="max-h-full !w-[31.25%] relative ml-[100px]" />
-            <div className="flex flex-col !h-[400px] !w-[600px] font-[family-name:var(--font-geist-mono)]">
-              <span className="text-4xl">Hi! I&apos;m Pedro Coelho</span>
-              <span className="text-6xl">
-                <span className="text-8xl font-[family-name:var(--font-jersey-10)]">Game</span> developer
+          <div className="flex xl:!flex-row flex-col xl:gap-[250px] h-full overflow-hidden items-center">
+            <div className='h-full xl:max-w-[50%] xl:overflow-auto xl:ml-[100px] overflow-hidden'>
+              <Image alt="" src={ProImage} width={0} height={0} className="xl:h-[100%] xl:w-[calc(100%*1.5)] relative" />
+            </div>
+            <div className="flex flex-col xl:!h-[400px] xl:p-0 p-[100px] h-full xl:!w-[600px] w-full xl:!text-lg text-6xl font-[family-name:var(--font-geist-mono)] text-justify ">
+              <span className="xl:!text-4xl text-6xl">Hi! I&apos;m Pedro Coelho</span>
+              <span className="xl:!text-6xl text-[200px]">
+                <span className="xl:!text-7xl text-[256px] font-[family-name:var(--font-jersey-10)]">Game</span> developer
               </span>
               Passionate and skilled C# developer with extensive experience in advanced game
               development, specializing in both 2D and 3D projects. Proficient in implementing
               design patterns to create efficient, scalable, and maintainable code. Adept at
               identifying and delivering practical solutions to enhance project outcomes and
               optimize workflows.
-              <div className="flex gap-20 mt-10">
-                <button onClick={() => router.push("projects")} className="transform active:scale-90 flex items-center justify-center gap-3 w-60 px-4 py-2 bg-[#396291] text-white rounded-lg hover:bg-[#2c4e75] transition cursor-pointer">
-                  Projects <Code className=" text-gray-200" />
+              <div className="flex xl:gap-20 gap-0 xl:justify-center justify-between xl:mt-10 mt-auto xl:h-auto h-[150px]">
+                <button onClick={() => router.push("projects")} className="transform active:scale-90 flex items-center justify-center xl:gap-3 gap-8 xl:w-60 w-[650px] px-4 py-2 bg-[#396291] text-white xl:rounded-lg rounded-[24px] hover:bg-[#2c4e75] transition cursor-pointer xl:text-xl text-5xl">
+                  Projects <Code className=" text-gray-200 xl:w-[22px] xl:h-[22px] h-[70px] w-[80px]" />
                 </button>
-                <button className="transform active:scale-90 flex items-center justify-center gap-3 w-60 px-4 py-2 border-4 border-[#396291] text-[#396291] rounded-lg hover:border-[#2c4e75] hover:bg-[#2c4e75] hover:text-white transition cursor-pointer" onClick={() => handleButtonClick()}>
-                  Hire-me <SquareArrowOutUpRight />
+                <button className="transform active:scale-90 flex items-center justify-center xl:gap-3 gap-8 xl:w-60 w-[650px] px-4 py-2 xl:border-4 border-8 border-[#396291] text-[#396291] xl:rounded-lg rounded-[24px] hover:border-[#2c4e75] hover:bg-[#2c4e75] hover:text-white transition cursor-pointer xl:text-xl text-5xl font-bold" onClick={() => handleButtonClick()}>
+                  Hire-me <SquareArrowOutUpRight className="xl:w-[22px] xl:h-[22px] h-[70px] w-[70px]" />
                 </button>
               </div>
             </div>
--- src/app/projects/components/project-card.tsx
+++ src/app/projects/components/project-card.tsx
@@ -9,10 +9,10 @@ import { DialogData } from '../utilsClasses';
 
 export const teste = tv({
   slots: {
-    card: "group flex flex-col-reverse mx-3 w-[500px] h-[750px] min-w-[500px] min-h-[300px] transition-all duration-180 bg-cover hover:cursor-pointer hover:h-[800px] hover:z-[1000] rounded-2xl bg-no-repeat bg-center",
+    card: "group flex flex-col-reverse mx-3 xl:w-[500px] w-[1500px] xl:h-[750px] h-[2250px] xl:min-w-[500px] xl:min-h-[300px] min-w-[1500px] min-h-[2250px] transition-all duration-180 bg-cover hover:cursor-pointer hover:h-[800px] hover:z-[1000] rounded-2xl bg-no-repeat bg-center",
     cardBlur: "relative before:content-[''] before:rounded-2xl before:absolute before:inset-0 before:bg-black/70 before:backdrop-blur-sm before:opacity-0 before:transition-opacity before:duration-180 hover:before:opacity-100",
     titleClass: "absolute w-full text-center bottom-[8px] left-1/2 -translate-x-1/2  text-white  font-[family-name:var(--font-jersey-10)] text-6xl opacity-100 transition-all duration-180 group-hover:translate-y-[-400px]",
-    dialogTitleClass: "mx-auto font-[family-name:var(--font-jersey-10)] text-7xl mt-[75px] text-white",
+    dialogTitleClass: "mx-auto font-[family-name:var(--font-jersey-10)] xl:text-7xl text-9xl mt-[75px] text-white",
     descriptionClass: "text-white  indent-[30px] font-[family-name:var(--font-geist-mono)] opacity-0 transition-all duration-180  group-hover:opacity-100 mx-[auto] mt-[22px] text-[20px]  w-[calc(100%*1/2*1.7777)] text-justify scroll-auto",
     descriptionArea: "relative flex flex-col h-full",
     descriptionImageContainer: "opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 relative w-[calc(100%*1/2*1.7777)] min-h-[calc(100%*1/1.5*0.5625)] m-auto mb-0 mt-[12px] rounded-md transition-all ease-in-out overflow-hidden",
@@ -50,25 +50,20 @@ export default function ProjectCard({ title, description, imageUrl, cardImageUrl
           <span className={`${descriptionImageContainer()} ${borderGradient()}`}>
             <Image className={`${descriptionImage()}`} src={cardImageUrl} alt="" width={1920} height={1080}/>
           </span>
-
           <SimpleBar className="max-h-[350px] mt-[72px]">
-            <p className={descriptionClass()}>
-              {description}
-            </p>
+            <p className={descriptionClass()}>{description}</p>
           </SimpleBar>
-
-
           <span className={titleClass()}>{title}</span>
         </div>
       </div>
       <Dialog isOpen={isDialogOpen} onClose={() => { setIsDialogOpen(false) }} bgImage={imageUrl}>
         <div className="flex flex-col w-full h-full overflow-y-scroll overflow-x-hidden " >
           {dialogData.map((item, index) => (
-            <div key={index} className='flex w-full !min-h-full h-full z-10'>
-              <span className='w-[33%] h-full flex ml-[8%]'>
-                <span className='w-[100%] bg-black/40 flex flex-col gap-4	'>
+            <div key={index} className='flex xl:flex-row flex-col xl:items-start items-center w-full !min-h-full h-full z-10'>
+              <span className='xl:inline relative xl:w-[33%] w-[90%] h-full flex xl:ml-[8%]'>
+                <span className='w-[100%] absolute top-0 bottom-[-100%] bg-black/40 flex flex-col gap-4	z-[-1]'>
                   <span className={dialogTitleClass()}>{title}</span>
-                  <span className=' font-[family-name:var(--font-geist-mono)] text-[22px] text-white px-[22px] text-justify'>
+                  <span className=' font-[family-name:var(--font-geist-mono)] xl:text-[22px] text-[66px] text-white px-[22px] text-justify'>
                     <div className='indent-[30px] '>{item.firstText}</div>
                     <br/>
                     <div className='indent-[30px]'>{item.secondText}</div>
@@ -77,12 +72,12 @@ export default function ProjectCard({ title, description, imageUrl, cardImageUrl
                   </span>
                 </span>
               </span>
-              <span className='flex flex-col gap-4 w-[67%] h-full justify-center items-center'>
-                <span className='grid grid-cols-[615px_288px] gap-4 h-[345px] overflow-hidden'>
-                  <Image  alt="" src={item.mainImage} width={1920} height={1080} className="relative rounded-md shadow-lg  !h-[345px] object-cover" />
+              <span className='flex flex-col gap-4 xl:w-[67%] w-full h-full justify-center items-center'>
+                <span className='grid xl:grid-cols-[615px_288px] grid-cols-[984px_461px] gap-4 xl:h-[345px] h-[552] overflow-hidden'>
+                  <Image  alt="" src={item.mainImage} width={1920} height={1080} className="relative rounded-md shadow-lg  xl:!h-[345px] !h-[552] object-cover" />
                   <Image alt="" src={item.upperMinorImage} width={1920} height={1080} className="relative rounded-md shadow-lg bottom-2.5 !w-[full]" />
                 </span>
-                <span className='grid grid-cols-[288px_615px] gap-4 h-[345px] overflow-hidden'>
+                <span className='grid xl:grid-cols-[288px_615px] grid-cols-[461px_984px] gap-4 xl:h-[345px] h-[552] overflow-hidden'>
                   <Image alt="" src={item.bottomMinorImage} width={1920} height={1080} className="relative rounded-md shadow-lg bottom-2.5 !w-full " />
                   <video src={item.videoSrc}
                     autoPlay
