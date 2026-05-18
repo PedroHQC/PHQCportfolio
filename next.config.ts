9889cfc71912e88cdad20dd51f260f942821b49c
pedroHQC <pedrohenrique2.0583@gmail.com>
2026-05-18 01:16:46 -0300

    ajuste

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   output: isProd ? "export" : undefined,
-  basePath: isProd ? "/PHQCportfolio" : undefined,
-  assetPrefix: isProd ? "/PHQCportfolio/" : undefined,
+  basePath: isProd ? "" : undefined,
+  assetPrefix: isProd ? "" : undefined,
   images: {
     unoptimized: true,
   },
